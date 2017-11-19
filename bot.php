4fe27b01e194bb5f5e59b4922bbdd8aa471b24ba
serversuck <33795437+serversuck@users.noreply.github.com>
2017-11-19 10:06:48 +0700

    Update bot.php

--- bot.php
+++ bot.php
@@ -17,7 +17,7 @@ if (!is_null($events['events'])) {
 			//http://110.164.57.36/voody/reply.php?keyword=voody
 			$url = "http://110.164.57.36/voody/reply.php?keyword=voody max";
 			//echo 
-			$msgreply = $keyword; 
+			$msgreply = $url; 
 			//$text = "i will learn from u";
 			// Get replyToken
 			
