ddf9d4600d639e865de9f6f50b07eca721121afc
serversuck <33795437+serversuck@users.noreply.github.com>
2018-02-09 10:02:53 +0700

    Create test.php

---
+++ test.php
@@ -0,0 +1,86 @@
+<?php
+$access_token = 'DnkJV7DuTpajvt+bbha+hBzZOcVT3TGYrZY8qfwOCgBfkN4t+u5Y4h31CfC9MBVs2tA+nHn23CrizHiTHZsA+rjXKfVQ/CjzOnwzfJBM2yYpTa+CmrQP53tEEZmEMwrHPxzlaslZ+GABub8tE+EhUgdB04t89/1O/w1cDnyilFU=';
+
+function getHtml($url, $post = null) {
+    $ch = curl_init();
+    curl_setopt($ch, CURLOPT_URL, $url);
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+    curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
+    curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+    curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, 0);
+    if(!empty($post)) {
+        curl_setopt($ch, CURLOPT_POST, true);
+        curl_setopt($ch, CURLOPT_POSTFIELDS, $post);
+    } 
+    $result = curl_exec($ch);
+    curl_close($ch);
+    return $result;
+}
+
+
+// Get POST body content
+$content = file_get_contents('php://input');
+// Parse JSON
+$events = json_decode($content, true);
+// Validate parsed JSON data
+if (!is_null($events['events'])) {
+	// Loop through each event
+	foreach ($events['events'] as $event) {
+		// Reply only when message sent is in 'text' format
+		if ($event['type'] == 'message' && $event['message']['type'] == 'text') {
+			// Get text sent
+			//$text = $event['message']['text'];
+			$keyword = $event['message']['text'];
+				if ((preg_match('/vd/',$keyword)) || (preg_match('/Vd/',$keyword))   || (preg_match('/voody/',$keyword)) || (preg_match('/Voody/',$keyword))){
+				//is ok to talk to voody
+				}else{
+				//echo "not";
+				exit();
+				}
+			
+			
+			
+			
+			
+			$keyword = str_replace(' ', '', trim($keyword));
+			//http://110.164.57.36/voody/reply.php?keyword=voody
+			$url = "http://110.164.57.36/voody/reply.php?keyword=".$keyword;
+			//echo 
+			$msgreply = getHtml($url); 
+			//$text = "i will learn from u";
+			// Get replyToken
+			
+			
+			$replyToken = $event['replyToken'];
+
+			// Build message to reply back
+			$messages = [
+				'type' => 'text',
+				'text' => $msgreply
+			];
+
+			// Make a POST Request to Messaging API to reply to sender
+			$url = 'https://api.line.me/v2/bot/message/reply';
+			$data = [
+				'replyToken' => $replyToken,
+				'messages' => [$messages],
+			];
+			$post = json_encode($data);
+			$headers = array('Content-Type: application/json', 'Authorization: Bearer ' . $access_token);
+
+			$ch = curl_init($url);
+			curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "POST");
+			curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+			curl_setopt($ch, CURLOPT_POSTFIELDS, $post);
+			curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
+			curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
+			$result = curl_exec($ch);
+			curl_close($ch);
+
+			echo $result . "\r\n";
+		}
+	}
+}
+echo "OK";
+
+?>
